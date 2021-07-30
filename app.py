b6450633e9b65151d5c30d593abb2deec9b7a01c
Josh Cormier <you@example.com>
2021-07-30 03:43:44 -0400

    added cors to app

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask, request, jsonify
-app = Flask(__name__)
+from  flask_cors import CORS
 from Pokemon import Pokemon
 from scraper import BulbapediaScraper
 from scraper import GEN_LIST
 from Stats import Stats
 from Nature import Nature
 
+app = Flask(__name__)
+CORS(app)
 @app.route("/calcIV", methods=["Post"])
 def calc_ivs():
         content = request.json
